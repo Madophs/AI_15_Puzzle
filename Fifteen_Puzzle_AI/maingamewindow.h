85482dc2ae5655a021d26e2cd913cd47b0e130fe
Madophs <madophs@madophs.com>
2019-08-16 15:19:33 -0500

    Random clicks

--- Fifteen_Puzzle_AI/maingamewindow.h
+++ Fifteen_Puzzle_AI/maingamewindow.h
@@ -10,6 +10,12 @@
 #include <cmath>
 #include <vector>
 #include <unordered_map>
+#include <random>
+#include <ctime>
+#include <algorithm>
+#include <thread>
+#include <chrono>
+#include <iostream>
 
 using namespace std;
 typedef unsigned int unint;
@@ -26,6 +32,7 @@ public:
     explicit MainGameWindow(QWidget *parent = nullptr);
     ~MainGameWindow();
     void addButtonsToGrid(){
+        vector<int> randomNumbers = generateRandomValues();
         unint buttonId = 0;
         QWidget *centralWidget = new QWidget(this);
         centralWidget->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
@@ -35,9 +42,16 @@ public:
             for(unint j = 0; j < cols; j++){
                 QPushButton *tmpButton = new QPushButton(this);
                 tmpButton->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-                tmpButton->setText(static_cast<QString>(to_string(++buttonId).c_str()));
-                if(i+1 == rows && j+1 == cols)
-                    tmpButton->setVisible(true);
+                tmpButton->setText(static_cast<QString>(to_string(randomNumbers[buttonId++]).c_str()));
+                if(randomNumbers[buttonId-1] == rows*cols){
+                    initialHiddenButtonIndexes = {i,j};
+                    QPalette palette = tmpButton->palette();
+                    palette.setColor(QPalette::Button,QColor(Qt::transparent));
+                    tmpButton->setAutoFillBackground(true);
+                    tmpButton->setPalette(palette);
+                    //tmpButton->setVisible(false);
+                    tmpButton->update();
+                }
                 hashTable[tmpButton->text().toStdString()] = make_pair(i,j);
                 buttons[i].push_back(tmpButton);
                 QObject::connect(buttons[i][j], SIGNAL(clicked()), this, SLOT(movementButtonAnimation()));
@@ -45,7 +59,12 @@ public:
             }
         }
         setCentralWidget(centralWidget);
+
     }
+    void hideEmptyButton(){
+        buttons[initialHiddenButtonIndexes.first][initialHiddenButtonIndexes.second]->setVisible(false);
+    }
+
 public slots:
     void movementButtonAnimation(){
         string buttonId = static_cast<QPushButton*>(sender())->text().toStdString();
@@ -67,16 +86,32 @@ public slots:
             hashTable.find(buttonId)->second = {i+direction.first, j+direction.second};
             hashTable.find(buttons[i+direction.first][j+direction.second]->text().toStdString())->second = {i,j};
             swap(buttons[i][j], buttons[i+direction.first][j+direction.second]);
-            visibleButtonAnimation->start();
             nonVisibleButtonAnimation->start();
+            visibleButtonAnimation->start();
         }
+    }
 
+    vector<vector<QPushButton*>> &getButtons(){
+        return buttons;
+    }
+
+    void randomClicks(){
+        srand(time(0));
+        for(int times = 10000; times--;){
+            this_thread::sleep_for(chrono::microseconds(300));
+            int i = (int)rand()%rows;
+            int j = (int)rand()%cols;
+            //w.triggerClickedButtonEvent(i,j);
+            buttons[i][j]->clicked(true);
+            cout<<i<<" "<<j<<endl;
+        }
     }
 private:
     Ui::MainGameWindow *ui;
     QGridLayout *layout;
     QPropertyAnimation *visibleButtonAnimation{nullptr}, *nonVisibleButtonAnimation{nullptr};
     vector<vector<QPushButton*>> buttons;
+    pair<int,int> initialHiddenButtonIndexes{0,0};
     unordered_map<string, pair<unint, unint>> hashTable;
     unint rows{4}, cols{4};
     int animationDuration{100};
@@ -92,6 +127,13 @@ private:
             return {0,-1};
         }
         return {0,0};
+    }
+    vector<int> generateRandomValues(){
+        srand(time(0));
+        vector<int> randomNumbers(rows * cols);
+        iota(randomNumbers.begin(), randomNumbers.end(), 1);
+        random_shuffle(randomNumbers.begin(), randomNumbers.end());
+        return randomNumbers;
     }
 };
 
